885875564556f590cf2ed6bcebe1bfb571a5a4c1
amaniesseili <esseiliamani@hotmail.com>
2023-07-12 23:45:39 +0200

    generare array di 16 numeri random da 1 a 100

--- main.js
+++ main.js
@@ -20,4 +20,15 @@ function onSquareClick ( ) {
     this.classList.toggle("bg-success"); 
 }
 
+// ---------------------------------
 
+// genero 16 numeri random da 1 a 100 
+//const randomNumber = Math.floor(Math.random() * 100) +1; questo genera solo un numero 
+
+
+const randomNumbers = [];
+for (let i=0; i<16; i++){
+    const randomNum = randomNumber = Math.floor(Math.random() * 100) +1;
+    randomNumbers.push(randomNum) 
+}
+console.log(randomNumbers)
