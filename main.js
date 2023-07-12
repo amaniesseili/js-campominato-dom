d0f6cff05a5e01de332e17fb6f0585bdd717f199
amaniesseili <esseiliamani@hotmail.com>
2023-07-12 23:08:36 +0200

    on click il quadrato e verde

--- main.js
+++ main.js
@@ -4,5 +4,20 @@ for(let i=0; i<100; i++){
     const square = document.createElement("div");
     square.classList.add("grid-square");
 
+    square.addEventListener("click", onSquareClick) //dichiaro la funzione onclick..
+
+
+
+
+
+
     gridContainer.append(square);
 }
+
+
+//invoco la la funzione onClick fuori del ciclo for 
+function onSquareClick ( ) {
+    this.classList.toggle("bg-success"); 
+}
+
+
