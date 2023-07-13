072d5089f7a76817dd535f7e3e1ca0ab5fe16eb3
amaniesseili <esseiliamani@hotmail.com>
2023-07-13 17:07:08 +0200

    logica della bomba fatta

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 const gridContainer = document.querySelector(".grid-container")
-const bombs = [];
+
 const numBombs = 16;
 
-for(let i=0; i<100; i++){
+const bombs = generateBombs()
+console.log(bombs)
+
+for(let i=1; i<=100; i++){
     const square = document.createElement("div");
     square.classList.add("grid-square");
 
-    square.addEventListener("click", onSquareClick) //dichiaro la funzione onclick..
+    square.addEventListener("click", function(){
+        this.classList.toggle("bg-success"); 
+
+    const squareIndex = [];
+
+
+    if (bombs.includes(i)) { //trovato una bomba
+       
+       // this.classList.add("bg-danger"); 
+       console.log (" bomba");
+        
+    } else {
+        //this.classList.add("bg-success");
+        console.log("no bomba")
+        
+    }
+
+    }) //dichiaro la funzione onclick..
 
 
     gridContainer.append(square);
@@ -15,27 +35,18 @@ for(let i=0; i<100; i++){
 
 //randombombs
 function generateBombs() {
+    const bombs = [];
     for (let i = 0; i < 16; i++) {
         const randomNum = Math.floor(Math.random() * 100);
         bombs.push(randomNum);
     }
+    return bombs
 }
 
 
 //invoco la la funzione onClick fuori del ciclo for 
 function onSquareClick ( ) {
-    this.classList.toggle("bg-success"); 
-
-    const squareIndex = [];
-
-
-    if (bombs.includes(squareIndex)) {
-        squareIndex.push(i);
-        this.classList.add("bg-danger");
-        
-    } else {
-        this.classList.add("bg-success");
-    }
+    
 }
     
 
